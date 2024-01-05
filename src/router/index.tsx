9cb444bab4dc60dcefa4823ff758ee525a9eed3f
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-01-05 15:43:53 +0300

    first

---
+++ src/router/index.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useRoutes } from "react-router-dom";
+
+import { MainLayout } from "../layouts";
+import { Home } from "../pages";
+export const Router = () => {
+  return useRoutes([
+    {
+      path: "",
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "*", element: <Navigate to="/" replace /> },
+      ],
+    },
+  ]);
+};
