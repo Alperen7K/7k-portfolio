8b9f541fe4a99de64ee6958dcc2a4f18f57dc014
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-01-27 19:55:43 +0300

    finito

--- src/sections/Home/One.tsx
+++ src/sections/Home/One.tsx
@@ -9,10 +9,10 @@ export const One = () => {
     <div className="w-[100vw] z-20 h-[100vh] absolute pt-[100px] flex  justify-center bottom-0">
      <div className="flex flex-col lg:flex-row-reverse items-center lg:items-center lg:justify-evenly h-fit w-full   ">
       <div
-       className="w-[40vh] lg:w-[30vw] h-[40vh] lg:h-[30vw] bg-center mt-8 bg-no-repeat bg-cover rounded-full border-[15px] border-solid border-gri"
+       className="w-[40vh] lg:w-[30vw] h-[40vh] lg:h-[30vw] bg-center mt-20 bg-no-repeat bg-cover rounded-full border-[15px] border-solid border-gri lg:border-gri"
        style={{ backgroundImage: `url(${alpi})` }}
       />
-      <div className="lg:px-8 lg:py-4 lg:bg-gri rounded-full lg:border-[6px] border-solid border-white">
+      <div className="lg:px-8 lg:py-4 lg:bg-gri rounded-full ">
        <h1 className="text-center mt-16 lg:mt-0  text-[32px] font-bold">
         Mehmet Alperen YEDİK
        </h1>
