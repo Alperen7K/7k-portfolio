cb24432146000ccc29f6802fc8217c4ad47f093d
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-02-10 21:41:51 +0300

    update

--- src/pages/PortfolioInfo.tsx
+++ src/pages/PortfolioInfo.tsx
@@ -1,11 +1,19 @@
 import { useLocation } from "react-router-dom";
 import { data } from "../models/portfolio";
 import { Carousel, ConfigProvider } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export const PortfolioInfo = () => {
- const location = useLocation();
- const product = location.state;
+export const PortfolioInfo = ({ path }: { path: string }) => {
+ //  const location = useLocation();
+ //  const product = location.state;
+ const [Project, setProject] = useState<any>({
+  path: "",
+  name: "",
+  duty: "",
+  techs: [""],
+  detail: "",
+  photos: [],
+ });
 
  const scrollToTop = () => {
   window.scrollTo({
@@ -15,14 +23,22 @@ export const PortfolioInfo = () => {
  };
 
  useEffect(() => {
+  const FindProject = () => {
+   let proj = data.filter((e: any) => e.path === path);
+   console.log(proj);
+   setProject(proj[0]);
+  };
+  FindProject();
+ }, [data]);
+
+ useEffect(() => {
   scrollToTop();
  }, []);
 
- console.log(product);
  return (
   <div className="w-[100vw] overflow-x-hidden flex flex-col items-center">
    <h1 className="w-full font-bold text-center text-[30px] xl:text-[40px] mb-5 ">
-    {product.name}
+    {Project.name}
    </h1>
    <div className="w-full ">
     <ConfigProvider
@@ -37,13 +53,13 @@ export const PortfolioInfo = () => {
      }}
     >
      <Carousel autoplaySpeed={4000} className="" autoplay infinite>
-      {product.photos.map((photo: any, index: number) => {
+      {Project.photos.map((photo: any, index: number) => {
        return (
         <div className="w-full md:px-20 lg:px-40 xl:px-60">
          <img
           src={photo}
           className=" md:border-2 border-solid border-white md:rounded-xl  "
-          alt={product.name + index}
+          alt={Project.name + index}
          />
          <div className="w-full h-[30px]" />
         </div>
@@ -53,12 +69,12 @@ export const PortfolioInfo = () => {
     </ConfigProvider>
    </div>
    <div className="w-full p-5 py-10 flex flex-col items-center">
-    <h1 className="w-full md:px-20 lg:px-40 xl:px-60">{product.detail}</h1>
+    <h1 className="w-full md:px-20 lg:px-40 xl:px-60">{Project.detail}</h1>
     <div className="py-5">
      <h1 className="text-center text-[24px] font-bold mb-2">
       Used Technologies
      </h1>
-     {product.techs.join(" / ")}
+     {Project.techs.join(" / ")}
     </div>
    </div>
   </div>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -12,7 +12,51 @@ export const Router = () => {
     { path: "/about", element: <About /> },
     { path: "/skills", element: <Skills /> },
     { path: "/portfolio", element: <Portfolio /> },
-    { path: "/portfolio/:project", element: <PortfolioInfo /> },
+    // { path: "/portfolio/:project", element: <PortfolioInfo /> },
+    {
+     path: "/portfolio/kapsul-forms",
+     element: <PortfolioInfo path={"kapsul-forms"} />,
+    },
+    {
+     path: "/portfolio/news-automation",
+     element: <PortfolioInfo path={"news-automation"} />,
+    },
+    {
+     path: "/portfolio/kapsul-scanner",
+     element: <PortfolioInfo path={"kapsul-scanner"} />,
+    },
+    {
+     path: "/portfolio/binance-bot",
+     element: <PortfolioInfo path={"binance-bot"} />,
+    },
+    {
+     path: "/portfolio/press",
+     element: <PortfolioInfo path={"press"} />,
+    },
+    {
+     path: "/portfolio/kapsul-web",
+     element: <PortfolioInfo path={"kapsul-web"} />,
+    },
+    {
+     path: "/portfolio/kapsul-overtime",
+     element: <PortfolioInfo path={"kapsul-overtime"} />,
+    },
+    {
+     path: "/portfolio/kapsul-login",
+     element: <PortfolioInfo path={"kapsul-login"} />,
+    },
+    {
+     path: "/portfolio/tp2ae",
+     element: <PortfolioInfo path={"tp2ae"} />,
+    },
+    {
+     path: "/portfolio/voice-asistant",
+     element: <PortfolioInfo path={"voice-asistant"} />,
+    },
+    {
+     path: "/portfolio/e-commerce",
+     element: <PortfolioInfo path={"e-commerce"} />,
+    },
     { path: "*", element: <Navigate to="/" replace /> },
    ],
   },
